7a8a747adf9dc83efa757a8a71d259ff8c75b4d2
NingAnMe <ninganme@qq.com>
2018-03-30 15:55:58 +0800

    修改文件：PEP8

--- dcc_plt_timeseries.py
+++ dcc_plt_timeseries.py
@@ -1,32 +1,33 @@
 # coding:utf-8
-'''
+"""
 Created on 2017年9月21日
 
 @author: wangpeng
-'''
-import os, sys, h5py
-from configobj import ConfigObj
-from PB import pb_time
-from dateutil.relativedelta import relativedelta
-import numpy as np
-from numpy import tile
-from DV import dv_plt
+"""
+import os
+import sys
 from datetime import datetime
+from configobj import ConfigObj
+
+import numpy as np
+
+from PB import pb_time
+from DV import dv_plt
+
 
 def DccDataRead(iFile):
-    '''
-    '''
-    ary = []
-    names = ['ymd', 'Avg', 'Med', 'Mod', 'dccFiles', 'dccPoint', 'dccPrecent', 'dccDim']
-    formats = ['object', 'f4' , 'f4' , 'f4' , 'i4' , 'i4' , 'i4' , 'i4']
+    names = ['ymd', 'Avg', 'Med', 'Mod', 'dccFiles',
+             'dccPoint', 'dccPrecent', 'dccDim']
+    formats = ['object', 'f4', 'f4', 'f4', 'i4', 'i4', 'i4', 'i4']
 
     # 加载txt文
-    ary = np.loadtxt(iFile,
-                  dtype={'names': tuple(names),
-                         'formats': tuple(formats)},
-                  skiprows=1, ndmin=1)
+    arys = np.loadtxt(iFile,
+                      dtype={'names': tuple(names),
+                             'formats': tuple(formats)},
+                      skiprows=1, ndmin=1)
 
-    return ary
+    return arys
+
 
 ########################### 主程序入口 ############################
 if __name__ == '__main__':
@@ -52,16 +53,15 @@ if __name__ == '__main__':
     # 载入配置文件
     inCfg = ConfigObj(cfgFile)
 
-
     if len(args) == 2:  # 需要跟俩个参数执行
         satFlag = args[0]
         str_time = args[1]
         date_s, date_e = pb_time.arg_str2date(str_time)
 
-#         # 卫星标识检查，配置中没有则不处理
-#         if satFlag not in inCfg.keys():
-#             print 'not support satellite: %s' % satFlag
-#             sys.exit(-1)
+        #         # 卫星标识检查，配置中没有则不处理
+        #         if satFlag not in inCfg.keys():
+        #             print 'not support satellite: %s' % satFlag
+        #             sys.exit(-1)
 
         ipath = inCfg['plt'][satFlag]['ipath']
         opath = inCfg['plt'][satFlag]['opath']
@@ -73,7 +73,8 @@ if __name__ == '__main__':
         # 拼接需要读取的文件
         for each in var:
             for ch in band:
-                FileName = 'DCC_%s_%s_%s_Rolldays_%s_ALL.txt' % (satFlag, each, ch, rollday)
+                FileName = 'DCC_%s_%s_%s_Rolldays_%s_ALL.txt' % (
+                    satFlag, each, ch, rollday)
                 DccFile = os.path.join(ipath, FileName)
                 print DccFile
                 ######### 一、读取dcc提取后的标准文件 ##############
@@ -93,40 +94,46 @@ if __name__ == '__main__':
                     MedATT = ary['Med'] / 100.
                     ModATT = ary['Mod'] / 100.
 
-
                 ######## 三、绘图   ###################
-                outPng_avg = os.path.join(opath, 'DCC_%s_%s_%s_Rolldays_%s_Timeseries_Avg.png' % (satFlag, each, ch, rollday))
-                outPng_med = os.path.join(opath, 'DCC_%s_%s_%s_Rolldays_%s_Timeseries_Med.png' % (satFlag, each, ch, rollday))
-                outPng_mod = os.path.join(opath, 'DCC_%s_%s_%s_Rolldays_%s_Timeseries_Mod.png' % (satFlag, each, ch, rollday))
+                outPng_avg = os.path.join(opath,
+                                          'DCC_%s_%s_%s_Rolldays_%s_Timeseries_Avg.png' % (
+                                              satFlag, each, ch, rollday))
+                outPng_med = os.path.join(opath,
+                                          'DCC_%s_%s_%s_Rolldays_%s_Timeseries_Med.png' % (
+                                              satFlag, each, ch, rollday))
+                outPng_mod = os.path.join(opath,
+                                          'DCC_%s_%s_%s_Rolldays_%s_Timeseries_Mod.png' % (
+                                              satFlag, each, ch, rollday))
                 x = []
                 for i in xrange(len(ary['ymd'])):
                     dtime = datetime.strptime(ary['ymd'][i], '%Y%m%d')
                     x.append(dtime)
                 p = dv_plt.dv_time_series(figsize=(8, 4))
                 p.fig.subplots_adjust(bottom=0.15)
-#                 p.y_fmt = "%0.3f"
-                p.easyplot(x, AvgATT, 'r', 'avg', marker='x-', markersize=3, mec="r")
+                #                 p.y_fmt = "%0.3f"
+                p.easyplot(x, AvgATT, 'r', 'avg', marker='x-', markersize=3,
+                           mec="r")
                 p.title = u'%s %s 时序图' % (satFlag, ch)
                 p.ylabel = u'%s Avg' % each
                 p.savefig(outPng_avg)
 
                 p = dv_plt.dv_time_series(figsize=(8, 4))
                 p.fig.subplots_adjust(bottom=0.15)
-#                 p.y_fmt = "%0.3f"
-                p.easyplot(x, MedATT, 'r', 'med', marker='x-', markersize=3, mec="r")
+                #                 p.y_fmt = "%0.3f"
+                p.easyplot(x, MedATT, 'r', 'med', marker='x-', markersize=3,
+                           mec="r")
                 p.title = u'%s %s 时序图' % (satFlag, ch)
                 p.ylabel = u'%s Med' % each
                 p.savefig(outPng_med)
 
                 p = dv_plt.dv_time_series(figsize=(8, 4))
                 p.fig.subplots_adjust(bottom=0.15)
-#                 p.y_fmt = "%0.3f"
-                p.easyplot(x, ModATT, 'r', 'mod', marker='x-', markersize=3, mec="r")
+                #                 p.y_fmt = "%0.3f"
+                p.easyplot(x, ModATT, 'r', 'mod', marker='x-', markersize=3,
+                           mec="r")
                 p.title = u'%s %s 时序图' % (satFlag, ch)
                 p.ylabel = u'%s Mod' % each
                 p.savefig(outPng_mod)
 
     else:  # 没有参数 输出帮助信息
         print help_info
-
-
